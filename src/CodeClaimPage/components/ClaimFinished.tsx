beaa95bafcbdaa9b7188f12724c5fe4449731c44
Sebas Favaron <sebastianfavaron@gmail.com>
2021-06-29 18:55:37 -0300

    Add date

--- src/CodeClaimPage/components/ClaimFinished.tsx
+++ src/CodeClaimPage/components/ClaimFinished.tsx
@@ -15,12 +15,29 @@ const ClaimFinished: React.FC<{ claim: HashClaim }> = ({ claim }) => {
   const claimedWithEmail = !!(claim && claim.claimed && claim.user_input && isValidEmail(claim.user_input));
 
   const appLink = claimedWithEmail ? `/scan/${claim.user_input}` : `/scan/${claim.beneficiary}`;
+  const calcDaysAgo = (date: string) => {
+    var today = new Date();
+    const dateParameter = new Date(date)
+    const isToday = dateParameter.getDate() === today.getDate() && dateParameter.getMonth() === today.getMonth() && dateParameter.getFullYear() === today.getFullYear();
+    if (isToday) {
+      return 0;
+    } else {
+      var daysAgo = Math.trunc((new Date().getTime() - new Date(claim.claimed_date).getTime()) / (24 * 60 * 60 * 1000))
+      daysAgo = daysAgo===0 ? 1 : daysAgo;
+      return daysAgo;
+    }
+  }
+  const daysAgo = calcDaysAgo(claim.claimed_date);
 
   return (
     <div className={'claim-info'} data-aos="fade-up" data-aos-delay="300">
       <div className={'info-title'}>
         Congratulations! <br />
-        {claim.event.name} badge is now in your wallet
+        {claim.event.name} badge is now in your wallet <br />
+        {
+          daysAgo >= 0 ? 
+          <span>This POAP was minted {daysAgo === 0 ? 'today' : daysAgo === 1 ? '1 day ago' : `${daysAgo} days ago`}</span> : null
+        }
       </div>
       <div className={'text-info'}>Keep growing your POAP collection!</div>
       <LinkButton text={'Check my badges'} link={appLink} extraClass={'link-btn'} />
